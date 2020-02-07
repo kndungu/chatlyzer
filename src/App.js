4bb57f2036e5ffe50e1762f02999bce026241856
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2020-02-07 18:17:48 +0300

    Store token

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -10,32 +10,58 @@ import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
-function App() {
-  return (
-    <Container>
-      <Row>
-        <Col className="sparkle-container">
-          <Image
-            src="https://assets.data.world/assets/new-sparkle-logo.da2d4c3dbd24c2b928c6c4cf41525872.png"
-            rounded
-          />
-          <Form className="token-input-container">
-            <Form.Row>
-              <Col>
-                <Form.Control
-                  type="password"
-                  placeholder="Enter your data.world token"
-                />
-              </Col>
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
-            </Form.Row>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
-  );
-}
+export default class App extends Component {
+  state = {
+    token: ""
+  };
 
-export default App;
+  handleTokenChange = e => {
+    const { value } = e.currentTarget;
+
+    this.setState({ token: value });
+  };
+
+  storeToken = (e) => {
+    e.preventDefault()
+    const { localStorage } = window;
+    const { token } = this.state;
+
+    localStorage.setItem("DW_API_TOKEN", token);
+  };
+
+  render() {
+    const { token } = this.state;
+
+    return (
+      <Container>
+        <Row>
+          <Col className="sparkle-container">
+            <Image
+              src="https://assets.data.world/assets/new-sparkle-logo.da2d4c3dbd24c2b928c6c4cf41525872.png"
+              rounded
+            />
+            <Form className="token-input-container">
+              <Form.Row>
+                <Col>
+                  <Form.Control
+                    type="password"
+                    placeholder="Enter your data.world token"
+                    value={token}
+                    onChange={this.handleTokenChange}
+                  />
+                </Col>
+                <Button
+                  variant="primary"
+                  type="submit"
+                  onClick={this.storeToken}
+                >
+                  Submit
+                </Button>
+              </Form.Row>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}

ef0f653f9e3e99be5f22165f9afa6dda3da22cca
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2020-02-07 17:59:38 +0300

    Add token input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col className="sparkle-container">
+          <Image
+            src="https://assets.data.world/assets/new-sparkle-logo.da2d4c3dbd24c2b928c6c4cf41525872.png"
+            rounded
+          />
+          <Form className="token-input-container">
+            <Form.Row>
+              <Col>
+                <Form.Control
+                  type="password"
+                  placeholder="Enter your data.world token"
+                />
+              </Col>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form.Row>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
